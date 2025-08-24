c6217555ce11d61548a55509bb2b4074845c1a0c
Axel Munoz <naclif30@gmail.com>
2025-08-24 19:32:32 -0400

    Login and Register Page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,47 @@
+import { Navbar } from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
 
-export default function Home() {
-  return (
-    <div className="">
-      <Link href="/gastos" >
-        <button>Ir a gastos hehe</button>
-      
-      </Link>
-    </div>
-  );
+export const Home = () => {
+
+  return(
+
+    <>
+
+      <Navbar/>
+      <div className="flex flex-col md:flex-row justify-center items-center px-6 min-h-screen">
+        {/* Texto */}
+        <div>  
+          <h1>HOLA AMIGOS</h1>
+        </div>
+
+        {/* Imagen */}
+        <div className="w-full md:w-1/2 flex justify-center mb-6 md:mb-0">
+
+          <Image
+            src="/images/punpun.jpg"
+            alt="Imagen de prueba"
+            width={200}        // Ancho en píxeles
+            height={100}
+            />
+        </div>
+
+        
+
+
+
+      </div>
+
+
+
+
+    
+    
+    </>
+
+  )
+
 }
+
+export default Home;
