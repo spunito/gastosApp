ca365745977a58664d4ae22d0553373f10b8959d
Axel Munoz <naclif30@gmail.com>
2025-08-25 20:56:49 -0400

    feat:backend initializate and register up

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+    
+    const { name, email, password } = await req.json();
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) return NextResponse.json({ message: "User already exists" }, { status: 400 });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Crear usuario
+    const user = await prisma.user.create({
+        data: { name, email, password: hashedPassword },
+    });
+
+    return NextResponse.json({
+        message: "Usuario creado",
+        user: { id: user.id, name: user.name, email: user.email }
+    }, { status: 201 });
+}
